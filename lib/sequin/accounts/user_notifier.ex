c71f84fc886e025432a101e8f1fbdb1632144a6d
Alexandre Krispin <development.s004k@simplelogin.com>
2025-12-16 16:25:58 -0300

    Improve subjects names

--- lib/sequin/accounts/user_notifier.ex
+++ lib/sequin/accounts/user_notifier.ex
@@ -25,7 +25,7 @@ defmodule Sequin.Accounts.UserNotifier do
   Deliver instructions to confirm account.
   """
   def deliver_user_registration_confirmation(user, password_url, app_url) do
-    deliver(user.email, "[Sequin] You have been added to our CDC platform", 22, %{
+    deliver(user.email, "You have been added to our CDC platform", 22, %{
       "name" => user.name,
       "password_url" => password_url,
       "app_url" => app_url
@@ -36,7 +36,7 @@ defmodule Sequin.Accounts.UserNotifier do
   Deliver instructions to confirm account.
   """
   def deliver_confirmation_instructions(user, url) do
-    deliver(user.email, "[Sequin] Please confirm your email", 24, %{
+    deliver(user.email, "Please confirm your email", 24, %{
       "name" => user.name,
       "user_email" => user.email,
       "confirmation_url" => url
@@ -47,7 +47,7 @@ defmodule Sequin.Accounts.UserNotifier do
   Deliver instructions to reset a user password.
   """
   def deliver_reset_password_instructions(user, url) do
-    deliver(user.email, "[Sequin] Reset your password", 23, %{
+    deliver(user.email, "Reset your password to access your CDC account", 23, %{
       "name" => user.name,
       "user_email" => user.email,
       "reset_password_url" => url
@@ -58,7 +58,7 @@ defmodule Sequin.Accounts.UserNotifier do
   Deliver instructions to update a user email.
   """
   def deliver_update_email_instructions(user, url) do
-    deliver(user.email, "[Sequin] Complete your update email request", 26, %{
+    deliver(user.email, "Complete your update email request", 26, %{
       "name" => user.name,
       "user_email" => user.email,
       "update_email_url" => url
@@ -66,7 +66,7 @@ defmodule Sequin.Accounts.UserNotifier do
   end
 
   def deliver_invite_to_account_instructions(send_to, inviting_user_email, account_name, url) do
-    deliver(send_to, "Invite to account instructions", 25, %{
+    deliver(send_to, "Invite to create your CDC account", 25, %{
       "inviting_user_email" => inviting_user_email,
       "account_name" => account_name,
       "url" => url
